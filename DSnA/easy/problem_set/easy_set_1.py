f786e79b81339cb361c8abbb9348af3bf617b1dc
Jeff Varela <Jeff.Varela@ombligo.local>
2023-05-16 14:32:18 -0400

    worked on DSnA

---
+++ DSnA/easy/problem_set/easy_set_1.py
@@ -0,0 +1,65 @@
+from audioop import reverse
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
+import sys
+import pprint
+from datetime import datetime 
+from inspect import getmembers, isfunction
+'''  
+The inspect module provides several useful functions to help get information about live objects such as modules, classes, methods, functions, tracebacks, frame objects, and code objects. For example, it can help you examine the contents of a class, retrieve the source code of a method, extract and format the argument list for a function, or get all the information you need to display a detailed traceback.
+
+The getmembers() function retrieves the members of an object such as a class or module. The functions whose names begin with “is” are mainly provided as convenient choices for the second argument to getmembers(). Return all the members in a list of (name, value). If the optional predicate argument—which will be called with the value object of each member—is supplied, only members for which the predicate returns a true value are included.
+'''
+
+
+def main():
+    ''' Main will ask you which algo to exec '''
+    print('Choose which algo to execute:\n')
+    public_func_list = [
+        member[0] for member in getmembers(sys.modules[__name__], isfunction) 
+        if member[0] != 'main' and member[0] != 'getmembers' and member[0] != 'isfunction' and member[0][0] != '_'
+    ]
+    dict_of_avail_funcs = dict(enumerate(public_func_list))
+    pprint.pprint(dict_of_avail_funcs) # prints a nicely formatted dict in terminal 
+    exec_func = int(input('\nPick a number from the dict: '))
+    print(f'\nExecuting order {exec_func}:\n')
+    eval(dict_of_avail_funcs[exec_func] + '()')
+
+
+
+def prob_00_mini_wait_time(): # RECORD_TIME:  BEST_TIME: 
+    ''' write a fn to find the longest common prefix str in a list[str], if no common prefix 
+    return empty str 
+	'''
+    res_1 = _longest_cp(["flower","flow","flight"]) 
+    res_2 = _longest_cp(["dog","racecar","car"]) 
+    print('pass' if res_1 == 'fl' else res_1)  #=> 'fl'
+    print('pass' if res_2 == '' else res_2) #=> ''
+
+
+def _longest_cp(words:List[str]) -> str:
+    '''
+    '''
+    cp = '' 
+    words = sorted(words) # sorts list[str] in lexicographical order 
+    first_word = words[0]
+    last_word = words[-1]
+
+    for idx in range(min(len(first_word), len(last_word))): # 
+        if first_word[idx] != last_word[idx]: 
+            return cp 
+        cp += first_word[idx]
+
+    return cp 
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('\n----------[ START ]----------\n')
+    main()
+    print('\n----------[ END ]----------\n')
