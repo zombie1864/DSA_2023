9a709dc0884d11c63d9fb6d0536d209568c1687b
jeff varela <zombie1864@gmail.com>
2023-07-27 15:20:41 -0400

    updated fico.py file

--- project_FICO.py
+++ project_FICO.py
@@ -1,51 +1,55 @@
 from typing import Any, Dict, Union, Tuple
 
-# be my financial planner 
 
 _capital_one_bank = {
     '1st_cd'        : (80000, 0.0415), 
     '2nd_cd'        : (10000, 0.05), 
-    'savings_acc'   : (30000, 0.0415)
+    'savings_acc'   : (30000, 0.0415) # rate changes over time
 }
-
-
 _robinhood = {
-    'brokerage_acc': (10000, 0.0465)
+    'brokerage_acc': (10000, 0.0465) # rate changes over time
 }
+_jeff__tot_contr = 1700 
+_tiff__tot_contr = 4600 
+_tot_contr = _jeff__tot_contr + _tiff__tot_contr 
+_monthly_payout = 1500 
 
 
-savings_contr = 4600 + 1400
 
+def interest_pymt_from(acc:Dict[str, Tuple], view_type:str) -> Union[float, int, str]: 
+    '''  Calc the tot interest pymt from an acc 
+        Returns the interest pymt amt either per month or per year 
+    '''
 
-def interest_pymt_from(acc, view_type:str): 
-    '''  '''
+    tot_interest_money:Union[float, int] = 0 
 
-    tot_interest_money = 0 
+    for balance in acc.values(): # acc:Dict[str, Tuple]
+        acc_val:Union[float, int]       = balance[0]
+        interest_rate:Union[float, int] = balance[1]
 
-    for acc_val in acc.values(): 
-        tot_interest_money += acc_val[0] * acc_val[1]
+        tot_interest_money += acc_val * interest_rate
 
-    tot_interest_money_per_month = round(tot_interest_money / 12, 2)
+    tot_interest_money_per_month:Union[float, int] = round(tot_interest_money / 12, 2)
 
     if view_type == 'per month': 
         return tot_interest_money_per_month 
     elif view_type == 'per year': 
         return tot_interest_money
     else: 
-        print('Not valid view type')
+        return 'Not valid view type'
 
 
 
 def update_account_balance_4(acc:Dict[str, Tuple], contr_freq:int=1) -> Dict[str, Tuple]:
-    # Create a new dictionary with the updated value for 'savings_acc'
+    ''' Create a new dictionary with the updated value for 'savings_acc' '''
 
-    updated_acc = {
-        key: ((contr_freq * savings_contr) + balance, rate) 
+    updated_acc:Dict[str, Tuple] = {
+        key : ((contr_freq * _tot_contr) + acc_val, rate) 
         if key == 'savings_acc' else 
-        (balance, rate) for key, (balance, rate) in acc.items()
+        (acc_val, rate) for key, (acc_val, rate) in acc.items()
     }
 
-    return updated_acc
+    return updated_acc # returns an updated acc 
 
 
 
@@ -69,12 +73,16 @@ def project(time:Union[float, int], unit:str) -> Union[int, float]:
 
 if __name__ == '__main__':
     print()
-    capital_one_interest_pymt = interest_pymt_from(_capital_one_bank, 'per month')
-    robinhood_interest_pymt = interest_pymt_from(_robinhood, 'per month')
-    tot_interest_pymt = capital_one_interest_pymt + robinhood_interest_pymt
-    print(f'{capital_one_interest_pymt} / month')
-    print(f'{robinhood_interest_pymt} / month')
-    print(f'Tot: {tot_interest_pymt} / month')
-    print(update_account_balance_4(_capital_one_bank, 19))
-    print(project(19,'month'))
-    print()
+    print('--Current Monthly Earnings--')
+    capital_one_interest_pymt   = interest_pymt_from(_capital_one_bank, 'per month')
+    robinhood_interest_pymt     = interest_pymt_from(_robinhood, 'per month')
+    tot_interest_pymt           = round(capital_one_interest_pymt + robinhood_interest_pymt, 2)
+    capital_one_msg             = f'Capital one payment: \t{capital_one_interest_pymt}'
+    robinhood_msg               = f'Robinhood payment: \t{robinhood_interest_pymt}'
+    tot_pyment_msg              = f'\nTotal payment: \t\t{tot_interest_pymt}\n'
+    print(capital_one_msg)
+    print(robinhood_msg)
+    print(tot_pyment_msg)
+    # print(update_account_balance_4(_capital_one_bank, 67))
+    # print(project(67,'month'))
+    print(f'Amt need to pay: {_monthly_payout - tot_interest_pymt} \nLeft over: {round(1700 - _monthly_payout + tot_interest_pymt, 2)}') 
