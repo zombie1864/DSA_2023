685f3287f2704b4ac84162b82adb988d5e1234c8
Jeff Varela <Jeff.Varela@ombligo.local>
2023-11-13 10:23:46 -0500

    updated core.js

--- core.js
+++ core.js
@@ -1,5 +1,5 @@
 /**
-@tutorial: JS PARADIGM AND USAGE 
+@tutorial   : JS PARADIGM AND USAGE 
 @description: Some major paradigm in js include: 
     []_event-driven: fns can be made to respond to events, such as `clicks` on el
     `resize` of the browsing window, `mousehover`, `mouseenter`, `mouseleave` etc. 
@@ -12,8 +12,9 @@
 **/
 
 
+
 /**
-@tutorial: VARIABLES AND SCOPING 
+@tutorial   : VARIABLES AND SCOPING 
 @description: `let`, `var`, `const`, block scope, fn scope, hoisting 
     []_`let` vs `var` {
         []_`let` used 4 block-scoped VAR 2 init val(s)
@@ -33,8 +34,9 @@
 **/
 
 
+
 /**
-@tutorial: ARRAY AND ARRAY BUILT-IN METHODS
+@tutorial   : ARRAY AND ARRAY BUILT-IN METHODS
 @description: Arr ds, itr and traversal methods, searching/finding methods, add/rm el(s), manipulating methods, 
 length and size methods, checking and conversion methods, cloning and copying methods, and Type-specific methods 
     []_arr are ds 4 storing info in a sequential order 
@@ -87,4 +89,101 @@ length and size methods, checking and conversion methods, cloning and copying me
         []_arr.of(...els)                    -- creates a new arr from its args 
         []_arr.length                        -- prop that specifies the num of els in an arr 
     }
+**/
+
+
+
+/**
+@tutorial   : The `switch` statement 
+@kind       : ADVANCE TOPIC
+@description: Boolean logic looks at cond in order 2 exec blocks of codes. A switch is similar but does not use 
+cond 2 compare against in order 2 exec blocks. Instead it compares val. 
+@example    : ```
+
+    let usrRank = 'private'; // default rank 
+    getUserRank(); // dev define fn that gets usr rank and mutates the val in VAR usrRank 
+
+    switch(usrRank) {
+        case 'sergent': 
+            print('You are not authorized. Please wait while I seek further assistance'); 
+            alertAdminOfAttempt(); // dev defined fn 
+            callCommander(); // dev defined fn 
+            setVideoInterface_Wcommander(); // dev defined fn 
+            getUserRank_WcommanderPermissionKey(); // dev defined fn 
+            break; 
+        case 'commander': 
+            print('Hello commander! what can i do for you today?'); 
+            promptCommanderRankOpt(); // dev defined fn 
+            break; 
+        case 'captain': 
+            print('Hello captain! I will do anything you wish.'); 
+            promptCaptainRankOpt(); // dev defined fn 
+            grantCaptainAdminAccessInterface(); // dev defined fn 
+            break; 
+        default: 
+            print('Greetings cadet. How can i help you?'); 
+            promptCadetOpt(); // dev defined fn 
+            alertAdminOfCadetUsage(); // dev defined fn 
+    }
+    
+``` 
+**/
+
+
+
+/**
+@tutorial   : `obj.hasOwnProperty(prop)`
+@kind       : ADVANCE TOPIC 
+@description: itr over an obj ds can yeild members who don't actually belong 2 the obj. The `obj.hasOwnProperty(prop)`
+is a built-in methid that checks if prop belongs 2 the obj and is not inherited from its prototype chain. 
+The prototype chain are props/methods that an object inherits from another ds class. In the case of the object ds 
+it inherits from the `Object` class. When you access a prop on an obj ds, js 1st checks if that prop exists 
+directly on the obj ds. If it doesn't then it looks up the prototype chain until it finds the prop. 
+
+    []_in js every inst of the `Object` class has a a bunch of built-in key-value pair containing meta info abt the obj {
+        @example: ``` 
+            let obj = {'fruits': ['apple']}
+        ``` 
+
+        []_list of prototype chain methods {
+            __proto__: ''
+            constructor: f Object() 
+            hasOwnProperty: f hasOwnPeropty() 
+            isPrototypeOf: f isPrototypeOf() 
+            propertyIsEnumerable: f propertyIsEnumerable() 
+            toLocaleString: f toLocaleString() 
+            toString: f toString() 
+            valueOf: f valueOf() 
+            ... 
+        }
+    }
+
+    
+
+    []_When itr over the props of an obj ds, it checks if the prop exist in the obj itself but if it doesn't find it 
+    it then looks into the prototype chain. If the obj mutates over time w/o direct dev knowledge this could led 2 bugs 
+
+
+    []_if you have full control of the lifecycle of the obj then you wouldn't need 2 use `obj.hasOwnProperty(prop)`. 
+    But if you do not then using the method would prevent potential bugs in the code 
+
+@example: ``` 
+    let fruitObj = {
+        'name': 'Apple', 
+        'shape': 'round', 
+        'taste': 'sweet' 
+    }; 
+    print(fruitObj.hasOwnProperty('name')) // => true 
+    print(fruitObh.hasOwnProperty('placeOfOrigins')) // => false. b.c. this is not a key in the obj 
+    print(fruitObh.hasOwnProperty('toString')) // => false. b.c. the obj itself does not have this method 
+    print(fruitObh.prototype.hasOwnProperty('toString')) // true b.c. the obj inherits from the `Object` class w this method 
+```
+
+@example: ```
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) { // always checks the obj itself and not the prototype chain 
+            print(`the val ${obj[key]} is in obj as prop ${key}`)
+        }
+    }
+``` 
 **/
